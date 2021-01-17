f2108b16a417ef91214ff829871d55eeb53e27dc
Seth Glenn <Sbgees@yahoo.com>
2021-01-17 16:38:20 -0600

    finished mongoose object for db management

--- Unsolved/models/workout.js
+++ Unsolved/models/workout.js
@@ -26,20 +26,23 @@ const workoutSchema = new Schema(
                     type: Number,
                     required: "Enter an exercise duration."
                 }, 
-                
-
-
-
-
-
-
+                weight: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                distance: {
+                    type: Number
+                }
             }
         ]
-
     },
+);
 
+const Workout = mongoose.model("Workout", workoutSchema);
 
-
-
-
-)
+module.exports = Workout;
