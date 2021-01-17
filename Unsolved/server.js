c0be9bbd3e1463515e7b11d385afc79f6ff4ef55
Seth Glenn <Sbgees@yahoo.com>
2021-01-17 17:35:24 -0600

    implementing mongoose and connection environments in server.js

--- Unsolved/server.js
+++ Unsolved/server.js
@@ -7,3 +7,15 @@ const PORT = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
+
+require("./routes/apiRoutes")(app);
+require
