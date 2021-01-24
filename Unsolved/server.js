f9ba7be9c2785f6e1de2e5a3ce61a7e8dde01c78
Seth Glenn <Sbgees@yahoo.com>
2021-01-23 21:21:51 -0600

    deploying to heroku

--- Unsolved/server.js
+++ Unsolved/server.js
@@ -16,6 +16,8 @@ app.use(express.static('public'));
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify: false
 })
 
