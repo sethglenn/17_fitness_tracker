e9c254c944814bf16f147d95c95191de70d89d33
Seth Glenn <Sbgees@yahoo.com>
2021-01-23 21:57:25 -0600

    implemented aggregate method to handle total workout duration information

--- Unsolved/routes/apiRoutes.js
+++ Unsolved/routes/apiRoutes.js
@@ -1,22 +1,26 @@
+const Workout = require("../models/workoutSchema");
 const db = require("../models/workoutSchema");
 
 module.exports = function (app) {
     app.get("/api/workouts", (_req, res) => {
-        db.find({}).then(data => {
-            res.json(data)
-        })
-        .catch(err => {
-            res.json(err)
-        })
+        Workout.aggregate([
+            { $addFields: { totalDuration: { $sum: '$exercises.duration' } } }
+        ])
+            .then((Workout) => {
+                res.json(Workout);
+            })
+            .catch((err) => {
+                res.json(err);
+            });
     });
     app.post("/api/workouts", (_req, res) => {
         db.create({}).then(data => {
             res.json(data)
         })
-        .catch(err =>{
-            console.log("err: >>", err)
-            res.json(err)
-        })
+            .catch(err => {
+                console.log("err: >>", err)
+                res.json(err)
+            })
     });
     app.put("/api/workouts/:id", ({ body, params }, res) => {
         db.findByIdAndUpdate(
@@ -28,6 +32,15 @@ module.exports = function (app) {
             .catch(err => {
                 console.log("err", err)
                 res.json(err)
-        })
+            })
     });
 };
+
+// app.get("/api/workouts", (_req, res) => {
+//     db.find({}).then(data => {
+//         res.json(data)
+//     })
+//     .catch(err => {
+//         res.json(err)
+//     })
+// });
